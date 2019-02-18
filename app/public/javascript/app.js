b5e8291315295b575315291eaa359a5260639eb4
Andrew Dawson <dawso.andrew@gmail.com>
2019-02-18 16:30:53 -0500

    Actually posts the data submitted from the form.

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -1,5 +1,25 @@
 "use strict";
 
+function ajaxCall(object) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open(object.method, object.url);
+    request.setRequestHeader("Content-Type", object.contentType);
+    request.onload = () => {
+      if (request.status === 200) {
+        return resolve(request.response);
+      } else {
+        return reject(Error(req.statusText));
+      }
+    };
+    request.onerror = (error) => {
+      return reject(Error("Network Error: " + error));
+    };
+    request.send(object.data);
+  });
+}
+
+
 const survey = document.getElementById("survey");
 
 survey.addEventListener("submit", (event) => {
@@ -11,6 +31,22 @@ survey.addEventListener("submit", (event) => {
   if (passedValidation === false) {
     event.stopPropagation();
   } else {
-    console.log("Placeholder for submission!");
+    const friend = {
+      name: document.getElementById("name").value,
+      photoLink: document.getElementById("photo-link").value,
+      scores: [
+        document.getElementById("question-1").value,
+      ],
+    };
+
+    ajaxCall({
+      contentType: "application/json",
+      data: JSON.stringify(friend),
+      method: "POST",
+      url: "/api/friends",
+    })
+    .then((response) => {
+      console.log(response);
+    });
   }
 });
